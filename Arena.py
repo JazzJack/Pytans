67aceb75bd911d1a7193d6bcf5cef481de29e699
pinae <pinae@web.de>
2011-10-30 19:52:44 +0100

    Arena update

--- Arena.py
+++ Arena.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python
 # encoding: utf-8
 from __future__ import division, print_function
-from rules.Character import Character
+from rules.Character import Character, readCharacterFromXML
 from rules.Maneuvers import *
 from rules.Weapons import *
+import Combat
+import Player
 import numpy as np
 
-horst = Character("Horst")
-horst.ST = 12
-horst.KO = 16
-horst.GE = 8
-horst.SN = 8
-horst.WT = 8
+class Arena:
+    def __init__(self):
+        self.teams = {}
 
+    def addTeam(self,name, characters):
+        self.teams[name] = characters
 
-adrian = Character("Adrian")
-adrian.ST = 8
-adrian.KO = 8
-adrian.GE = 16
-adrian.SN = 12
-adrian.WT = 15
+    def addCharacter(self, team, character):
+        self.teams[team].append(character)
 
+    def getCharacters(self):
+        characters = []
+        for team in self.teams:
+            characters.extend(team)
+        return characters
+
+# Create two characters
+char1 = readCharacterFromXML("res/Charaktere/Nostro.xml")
+char2 = readCharacterFromXML("res/Charaktere/Nostro.xml")
+arena = Arena()
+arena.addTeam("blue", [char1])
+arena.addTeam("red", [char2])
+# Create a Player for each character
+players = []
+for character in arena.getCharacters():
+    players.append("Player of "+character.name,Player(character))
+# Create a combat
+combat = Combat(arena, players)
+# fight till the death of one team
+combat.fightTillDefeat()
 
 print("Simulating 1000 %s by %s using %s..."%("Hiebe", "Adrian", "Gladius"))
 successes = []
---
+++ Combat.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+from __future__ import division, print_function
+import heapq
+
+class Combat:
+    def __init__(self, arena, players):
+        self.arena = arena
+        self.players = players
+
+    def combatRound(self):
+        # Determine the succession
+        succession = []
+        for player in self.players:
+            succession.append((-player.character.AP, player, True))
+        heapq.heapify(succession)
+        # Call the players
+        while len(succession):
+            playerTuple = heapq.heappop()
+            # Option to wait
+            if playerTuple[2]:
+                waittime = playerTuple[1].wait()
+                if waittime > 0:
+                    heapq.heappush((-playerTuple[1].character.AP, playerTuple[1], False))
+                    continue
+            # Act
+            playerTuple[1].act()
+
+    def oneTeamWins(self):
+        livingCharsInTheseTeams = set()
+        for team in self.arena.teams:
+            for character in self.arena.teams[team]:
+                if character.isAlive():
+                    livingCharsInTheseTeams.add(team)
+        return len(livingCharsInTheseTeams)<=1
+
+
+    def fightTillDefeat(self):
+        # Start combat
+        for player in self.players:
+            player.doInitiative()
+        # Simulate Rounds
+        while True:
+            self.combatRound()
+            if self.oneTeamWins():
+                return "One team wins!"
